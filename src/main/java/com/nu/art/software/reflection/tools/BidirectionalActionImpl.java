fa50af79a760d0d1582e677a7ec72548a83a4cc3
TacB0sS <Adam.Zehavi@nu-art-software.com>
2017-05-07 16:44:48 +0300

    Migrating from Bitbucket to Github git@bitbucket.org:nu-art-cyborg/cyborg-dev-project.git\nScary, first time in forever I would expose my stuff, and I think it is 2 years too late...\nI hope you guys will find this framework as useful as I intended it to be, I hope it will change the way people develop Android.\nLet's welcome Cyborg

---
+++ src/main/java/com/nu/art/software/reflection/tools/BidirectionalActionImpl.java
@@ -0,0 +1,144 @@
+/*
+ * The reflection project, is collection of reflection tools I've picked up
+ * along the way, use it wisely!
+ *
+ * Copyright (C) 2017  Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nu.art.software.reflection.tools;
+
+import com.nu.art.software.core.exceptions.InternalException;
+import com.nu.art.software.core.exceptions.MultiExceptions;
+
+import java.io.Serializable;
+import java.util.Vector;
+
+public class BidirectionalActionImpl<Type>
+		extends BidirectionalAction<Type>
+		implements Serializable {
+
+	public enum BidirectionalState {
+		Before,
+		After;
+
+		private BidirectionalState() { }
+	}
+
+	private static final long serialVersionUID = 957952667991035128L;
+
+	private Vector<Type> items = new Vector<>();
+
+	private BidirectionalState state = BidirectionalState.Before;
+
+	private boolean reverseOnFailure = true;
+
+	private String actionDescription;
+
+	public BidirectionalActionImpl(Class<Type> type, String actionDescription, String redoMethodName, Object[] redoParameters, String undoMethodName, Object[] undoParameters)
+			throws InternalException {
+		super(type, redoMethodName, redoParameters, undoMethodName, undoParameters);
+		this.actionDescription = actionDescription;
+	}
+
+	public void setReverseOnFailure(boolean reverseOnFailure) {
+		this.reverseOnFailure = reverseOnFailure;
+	}
+
+	public BidirectionalActionImpl(Class<Type> type, String actionDescription, String forwardMethod, String backwardMethod)
+			throws InternalException {
+		super(type, forwardMethod, backwardMethod);
+		this.actionDescription = actionDescription;
+	}
+
+	public void addItem(Type item)
+			throws InternalException {
+		if (item == null) {
+			return;
+		}
+		items.add(item);
+		if (state == BidirectionalState.After) {
+			try {
+				forwardInvocation.invokeMethod(item);
+			} catch (InternalException e) {
+				backwardInvocation.invokeMethod(item);
+			}
+		}
+	}
+
+	private void backward(int from, int until, Object[] parameters)
+			throws InternalException {
+		MultiExceptions me = new MultiExceptions("Un-Install Failed");
+		for (int i = from; i >= until; i--) {
+			try {
+				backwardInvocation.invokeMethod(items.get(i), parameters);
+			} catch (Exception e) {
+				me.addException(e);
+			}
+		}
+		me.process();
+	}
+
+	public final void backward(Object... parameters)
+			throws InternalException {
+		if (state == BidirectionalState.Before) {
+			return;
+		}
+		backward(items.size() - 1, 0, parameters);
+		state = BidirectionalState.Before;
+	}
+
+	public final void forward(Object... parameters)
+			throws InternalException {
+		if (state == BidirectionalState.After) {
+			return;
+		}
+		int i = 0;
+		try {
+			for (; i < items.size(); i++) {
+				forwardInvocation.invokeMethod(items.get(i), parameters);
+			}
+			state = BidirectionalState.After;
+		} catch (Exception e) {
+			MultiExceptions me = new MultiExceptions("Item: " + items.get(i) + " Action: " + actionDescription + " has Failed");
+			me.addException(e);
+			try {
+				if (reverseOnFailure) {
+					backward(i, 0);
+				}
+			} catch (Exception e1) {
+				me.addException(e);
+			}
+			me.process();
+		}
+	}
+
+	public BidirectionalState getState() {
+		return state;
+	}
+
+	public void removeItem(Type installable)
+			throws InternalException {
+		if (state == BidirectionalState.After) {
+			int index = items.indexOf(installable);
+			try {
+				backward(items.size() - 1, index);
+			} catch (InternalException e) {
+				items.remove(installable);
+				throw e;
+			}
+		}
+		items.remove(installable);
+	}
+}
---
+++ src/main/java/com/nu/art/software/reflection/tools/ReflectiveTools.java
@@ -0,0 +1,378 @@
+/*
+ * The reflection project, is collection of reflection tools I've picked up
+ * along the way, use it wisely!
+ *
+ * Copyright (C) 2017  Adam van der Kruk aka TacB0sS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nu.art.software.reflection.tools;
+
+import com.nu.art.software.core.exceptions.runtime.BadImplementationException;
+import com.nu.art.software.core.exceptions.runtime.ClassInstantiationRuntimeException;
+import com.nu.art.software.core.exceptions.runtime.MUST_NeverHappenedException;
+import com.nu.art.software.core.exceptions.runtime.ThisShouldNotHappenedException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ReflectiveTools {
+
+	/**
+	 * Creates a new instance of the supplied class type, via the default constructor.
+	 *
+	 * @param itemType The instance type to instantiate.
+	 * @return An instance of the supplied item type.
+	 */
+	public static <ItemType> ItemType newInstance(Class<ItemType> itemType) {
+		try {
+			Constructor<ItemType> constructor = itemType.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			ItemType item = constructor.newInstance();
+			constructor.setAccessible(false);
+			return item;
+		} catch (Exception e) {
+			throw new ClassInstantiationRuntimeException(itemType, e);
+		}
+	}
+
+	public static List<Class<?>> getAllInterfaces(Class<?> _class) {
+		if (_class == null) {
+			throw new BadImplementationException("");
+		}
+
+		List<Class<?>> interfacesFound = new ArrayList<>();
+		getAllInterfaces(_class, interfacesFound);
+		return interfacesFound;
+	}
+
+	private static void getAllInterfaces(Class<?> _class, List<Class<?>> interfacesFound) {
+		while (_class != null) {
+			Class<?>[] interfaces = _class.getInterfaces();
+
+			for (Class<?> anInterface : interfaces) {
+				if (!interfacesFound.contains(anInterface)) {
+					interfacesFound.add(anInterface);
+					getAllInterfaces(anInterface, interfacesFound);
+				}
+			}
+			_class = _class.getSuperclass();
+		}
+	}
+
+	public static Class<?>[] getInstancesTypes(Object[] instances) {
+		Class<?> parameterTypes[] = new Class<?>[instances.length];
+		int i = 0;
+		for (Object parameter : instances) {
+			parameterTypes[i++] = parameter.getClass();
+		}
+		return parameterTypes;
+	}
+
+	public static boolean isPrimitiveOrBoxed(Class<?> type) {
+		return isPrimitive(type) || isBoxed(type);
+	}
+
+	/* Primitives */
+	public static boolean isPrimitive(Class<?> type) {
+		return isNumericPrimitive(type) || type == boolean.class;
+	}
+
+	public static boolean isNumericPrimitive(Class<?> type) {
+		return isNaturalPrimitive(type) || isFloatingPrimitive(type);
+	}
+
+	public static boolean isNaturalPrimitive(Class<?> type) {
+		return type == int.class || type == short.class || type == char.class || type == long.class || type == byte.class;
+	}
+
+	public static boolean isFloatingPrimitive(Class<?> type) {
+		return type == float.class || type == double.class;
+	}
+
+	/* Boxed Primitives */
+	public static boolean isBoxed(Class<?> type) {
+		return isNumericBoxed(type) || type == Boolean.class || type == Character.class;
+	}
+
+	public static boolean isNumericBoxed(Class<?> type) {
+		return isNaturalBoxed(type) || isFloatingBoxed(type);
+	}
+
+	public static boolean isNaturalBoxed(Class<?> type) {
+		return type == Integer.class || type == Short.class || type == Long.class || type == Byte.class;
+	}
+
+	public static boolean isFloatingBoxed(Class<?> type) {
+		return type == Float.class || type == Double.class;
+	}
+
+	public static String parseInstancesParameterType(Object[] parameters) {
+		String toRet = "";
+		for (int i = 0; i < parameters.length; i++) {
+			toRet += parameters[i].getClass().getSimpleName();
+			if (i != parameters.length - 1) {
+				toRet += ", ";
+			}
+		}
+		return toRet;
+	}
+
+	public static String parseParametersType(Class<?>[] parameters) {
+		return parseParametersType(parameters, false);
+	}
+
+	public static String getMethodAsString(Method method) {
+		return getMethodAsString(method, false);
+	}
+
+	public static String getMethodAsString(Method method, boolean fullName) {
+		String parameters = parseParametersType(method.getParameterTypes(), fullName);
+		return method.getName() + "(" + parameters + ")";
+	}
+
+	public static String parseParametersType(Class<?>[] parameterTypes, boolean fullName) {
+		String toRet = "";
+		for (int i = 0; i < parameterTypes.length; i++) {
+			if (fullName) {
+				toRet += parameterTypes[i].getName();
+			} else {
+				toRet += parameterTypes[i].getSimpleName();
+			}
+			if (i != parameterTypes.length - 1) {
+				toRet += ", ";
+			}
+		}
+		return toRet;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <EnumType extends Enum<?>> EnumType[] getEnumValues(Class<EnumType> type) {
+		Method enumGetValues;
+		try {
+			enumGetValues = type.getDeclaredMethod("values");
+		} catch (Exception e) {
+			throw new MUST_NeverHappenedException("MUST NEVER HAPPEND: no values method for Enum!", e);
+		}
+		boolean wasAcessable = enumGetValues.isAccessible();
+		if (!wasAcessable) {
+			enumGetValues.setAccessible(true);
+		}
+		EnumType[] enumValues;
+		try {
+			enumValues = (EnumType[]) enumGetValues.invoke(null);
+		} catch (Exception e) {
+			throw new MUST_NeverHappenedException("MUST NEVER HAPPEND: no values method invocation error!", e);
+		} finally {
+			if (!wasAcessable) {
+				enumGetValues.setAccessible(false);
+			}
+		}
+		return enumValues;
+	}
+
+	public static <EnumType extends Enum<?>> EnumType getEnumFromValue(Class<EnumType> type, String value) {
+		EnumType[] enumValues = getEnumValues(type);
+		for (EnumType enumType : enumValues) {
+			if (enumType.name().equalsIgnoreCase(value)) {
+				return enumType;
+			}
+		}
+		throw new EnumConstantNotPresentException(type, value);
+	}
+
+	public static Class<?> getBoxedType(Class<?> type) {
+		if (type == Integer.class || type == int.class) {
+			return Integer.class;
+		}
+		if (type == Long.class || type == long.class) {
+			return Long.class;
+		}
+		if (type == Float.class || type == float.class) {
+			return Float.class;
+		}
+		if (type == Short.class || type == short.class) {
+			return Short.class;
+		}
+		if (type == Boolean.class || type == boolean.class) {
+			return Boolean.class;
+		}
+		if (type == Double.class || type == double.class) {
+			return Double.class;
+		}
+		if (type == Byte.class || type == byte.class) {
+			return Byte.class;
+		}
+		if (type == Character.class || type == char.class) {
+			return Character.class;
+		}
+		return type;
+	}
+
+	public static Class<?> getUnboxedType(Class<?> type) {
+		if (type == Integer.class || type == int.class) {
+			return int.class;
+		}
+		if (type == Long.class || type == long.class) {
+			return long.class;
+		}
+		if (type == Float.class || type == float.class) {
+			return float.class;
+		}
+		if (type == Short.class || type == short.class) {
+			return short.class;
+		}
+		if (type == Boolean.class || type == boolean.class) {
+			return boolean.class;
+		}
+		if (type == Double.class || type == double.class) {
+			return double.class;
+		}
+		if (type == Byte.class || type == byte.class) {
+			return byte.class;
+		}
+		if (type == Character.class || type == char.class) {
+			return char.class;
+		}
+		return type;
+	}
+
+	public static Object getDefaultValue(Class<?> type) {
+		if (type == String.class) {
+			return "";
+		}
+		if (type == Integer.class || type == int.class) {
+			return 0;
+		}
+		if (type == Long.class || type == long.class) {
+			return 0;
+		}
+		if (type == Float.class || type == float.class) {
+			return 0;
+		}
+		if (type == Short.class || type == short.class) {
+			return 0;
+		}
+		if (type == Boolean.class || type == boolean.class) {
+			return false;
+		}
+		if (type == Double.class || type == double.class) {
+			return 0;
+		}
+		if (type == Byte.class || type == byte.class) {
+			return 0;
+		}
+		if (type == Character.class || type == char.class) {
+			return 0;
+		}
+		return null;
+	}
+
+	public static Object getPrimitiveTypeInstance(Class<?> type, String value) {
+		if (value.length() == 0) {
+			return getDefaultValue(type);
+		}
+
+		if (type == String.class) {
+			return value;
+		}
+		if (type == Integer.class || type == int.class) {
+			return Integer.valueOf(value);
+		}
+		if (type == Long.class || type == long.class) {
+			return Long.valueOf(value);
+		}
+		if (type == Float.class || type == float.class) {
+			return Float.valueOf(value);
+		}
+		if (type == Short.class || type == short.class) {
+			return Short.valueOf(value);
+		}
+		if (type == Boolean.class || type == boolean.class) {
+			return Boolean.valueOf(value);
+		}
+		if (type == Double.class || type == double.class) {
+			return Double.valueOf(value);
+		}
+		if (type == Byte.class || type == byte.class) {
+			return Byte.valueOf(value);
+		}
+		throw new BadImplementationException("This method can only handle: int,long,float,short,boolean,double," + "byte. you have supplied: " + type
+				.getName());
+	}
+
+	public static HashMap<Object, Object> getFieldsCrossMappings(Class<?> resourceClass) {
+		return getFieldsCrossMappings(resourceClass, false);
+	}
+
+	public static HashMap<Object, Object> getFieldsCrossMappings(Class<?> resourceClass, boolean withParents) {
+		HashMap<Object, Object> resourceMap = new HashMap<>();
+		Class<?>[] interfaces = resourceClass.getInterfaces();
+		if (withParents)
+			for (Class<?> _interface : interfaces) {
+				resourceMap.putAll(getFieldsCrossMappings(_interface, withParents));
+			}
+		Field[] fields = resourceClass.getDeclaredFields();
+		for (Field field : fields) {
+			try {
+				boolean accessible = field.isAccessible();
+				if (!accessible) {
+					field.setAccessible(true);
+				}
+
+				Object value = field.get(null);
+				String name = (withParents ? resourceClass.getSimpleName() + "-" : "") + field.getName();
+
+				if (!accessible) {
+					field.setAccessible(false);
+				}
+
+				resourceMap.put(value, name);
+				resourceMap.put(name, value);
+			} catch (Exception e) {
+				throw new ThisShouldNotHappenedException("This should not happend", e);
+			}
+		}
+		return resourceMap;
+	}
+
+	public static Class<?> getPrimitiveClass(String type) {
+		if (type.equals("long"))
+			return long.class;
+
+		if (type.equals("int"))
+			return int.class;
+
+		if (type.equals("short"))
+			return short.class;
+
+		if (type.equals("byte"))
+			return byte.class;
+
+		if (type.equals("float"))
+			return float.class;
+
+		if (type.equals("double"))
+			return double.class;
+
+		if (type.equals("boolean"))
+			return boolean.class;
+
+		return null;
+	}
+}
